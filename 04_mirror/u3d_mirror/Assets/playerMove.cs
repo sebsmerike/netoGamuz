01b82571222020554edf3da2ad2e5c6fed2b97de
zevs <sebsmerike@users.noreply.github.com>
2025-05-30 00:00:45 -0600

    Se migra a la versión 6.0.x. Se agrega el movimiento de cámara y rotación con el teclado. Se sincronizan variables en red (SyncVar) junto con su _hook_. Se agregan los puntos de ''aparición'' (spawn).

--- 04_mirror/u3d_mirror/Assets/playerMove.cs
+++ 04_mirror/u3d_mirror/Assets/playerMove.cs
@@ -1,31 +1,88 @@
 using UnityEngine;
 using Mirror;
+using UnityEngine.UIElements;
 
 public class playerMove : NetworkBehaviour
 {
-    public float speed = 1.25f;
+    float speed = 1.25f;
+    // public Material mat;
 
-	// naming for easier debugging
-	public override void OnStartClient()
-	{
-		name = $"Player[{netId}|{(isLocalPlayer ? "local" : "remote")}]";
-		Debug.Log("OnStartClient: "+name);
-	}
+    public GameObject floating;
+    public TextMesh txtName;
 
-	public override void OnStartServer()
-	{
-		name = $"Player[{netId}|server]";
-		Debug.Log("OnStartServer: "+name);
-	}
+    private Material myMaterial;
 
-	void Update()
-	{
-		//if (!isLocalPlayer) return;
+    [SyncVar(hook = nameof(OnNameChange))]
+    public string playerName;
 
-		float h = Input.GetAxis("Horizontal");
-		float v = Input.GetAxis("Vertical");
+    void OnNameChange(string _old, string _new)
+    {
+        txtName.text = playerName;
+    }
 
-		Vector3 dir = new Vector3(h, 0, v);
-		transform.position += dir.normalized * (Time.deltaTime * speed);
-	}
+    [SyncVar(hook = nameof(OnColorChange))]
+    public Color playerColor;
+
+    void OnColorChange(Color _old, Color _new)
+    {
+        myMaterial = new Material(GetComponent<Renderer>().material);
+        myMaterial.color = _new;
+        GetComponent<Renderer>().material = myMaterial;
+
+        txtName.color = _new;
+    }
+
+    public void Start()
+    {
+        //mat.color = Color.gray;
+    }
+
+    // naming for easier debugging
+    public override void OnStartClient()
+    {
+        name = $"Player[{netId}|{(isLocalPlayer ? "local" : "remote")}]";
+        Debug.Log("OnStartClient: " + name);
+    }
+
+    public override void OnStartServer()
+    {
+        name = $"Player[{netId}|server]";
+        Debug.Log("OnStartServer: " + name);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //base.OnStartLocalPlayer();
+
+        Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+
+        string _name = "player_" + Random.Range(100, 999);
+        Color _color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        CmdSetupPlayer(_name, _color);
+    }
+
+    [Command]
+    public void CmdSetupPlayer (string newName, Color newColor)
+    {
+        playerName = newName;
+        playerColor = newColor;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            floating.transform.LookAt(Camera.main.transform);
+            return;
+        }
+
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4.0f;
+
+        transform.Rotate(0, moveX, 0);
+        transform.Translate(0, 0, moveZ);
+
+    }
 }
